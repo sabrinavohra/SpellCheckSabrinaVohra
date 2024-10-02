bf926cbb341379e272ed65b6cf4695edb5a166b2
sabrinavohra <sabrina.vohra@menloschool.org>
2024-10-02 13:17:56 -0700

    Worked on implementing tries. Uploading backed up files.

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -20,36 +20,51 @@ public class SpellCheck {
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
-        ArrayList<String> finalList = new ArrayList<>();
-        int start = 0;
-        int end = dictionary.length - 1;
-        for(String s: text) {
-            String current = found(s, dictionary, start, end);
-            if(current != null && !finalList.contains(current)) {
-                finalList.add(current);
-            }
+        Trie dictionaryList = new Trie();
+        for(int i = 0; i < dictionary.length; i++) {
+            dictionaryList
+            // Add each letter into the trie in the word's order using Trie methods
         }
-        String[] finalFound = new String[finalList.size()];
-        for(int i = 0; i < finalList.size(); i++) {
-            finalFound[i] = finalList.get(i);
-        }
-        return finalFound;
-    }
 
-    public String found(String s, String[] dictionary, int start, int end) {
-        int med = (end + start) / 2;
-        if(dictionary[med].equals(s)) {
-            return null;
+        Trie misspelled = new Trie();
+        for(int i = 0; i < text.length; i++) {
+            // If word is not in dictionary's trie and not already in the misspelled trie
+                // Insert into misspelled trie
+                // Remove word from text
+                    i--;
         }
-        else if(start >= end) {
-            return s;
-        }
-        else if(dictionary[med].compareTo(s) > 0) {
-            end = med - 1;
-        }
-        else if(dictionary[med].compareTo(s) < 0) {
-            start = med + 1;
-        }
-        return found(s, dictionary, start, end);
+        return text;
     }
+//        ArrayList<String> finalList = new ArrayList<>();
+//        int start = 0;
+//        int end = dictionary.length - 1;
+//        for(String s: text) {
+//            String current = found(s, dictionary, start, end);
+//            if(current != null && !finalList.contains(current)) {
+//                finalList.add(current);
+//            }
+//        }
+//        String[] finalFound = new String[finalList.size()];
+//        for(int i = 0; i < finalList.size(); i++) {
+//            finalFound[i] = finalList.get(i);
+//        }
+//        return finalFound;
+//    }
+//
+//    public String found(String s, String[] dictionary, int start, int end) {
+//        int med = (end + start) / 2;
+//        if(dictionary[med].equals(s)) {
+//            return null;
+//        }
+//        else if(start >= end) {
+//            return s;
+//        }
+//        else if(dictionary[med].compareTo(s) > 0) {
+//            end = med - 1;
+//        }
+//        else if(dictionary[med].compareTo(s) < 0) {
+//            start = med + 1;
+//        }
+//        return found(s, dictionary, start, end);
+//    }
 }
